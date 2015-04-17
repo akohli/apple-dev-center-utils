1743beb2a1d1d34348cf27f86d05ea28e9c90db9
Aman Kohli <aman.kohli@citi.com>
2015-04-17 15:58:24 +0100

    remove comment noise

--- expirable-apple-dev-asset.rb
+++ expirable-apple-dev-asset.rb
@@ -1,8 +1,11 @@
 require 'cupertino/provisioning_portal'
 
-# todo akohli 
-# put in gem wrap it etc 
-#
+# ensure $ios login is performed and no need to credentialise
+# in email provisioning bundle id, "profile name" and date of expiration
+
+#aget.username = "some.one@there.com"
+#aget.password = "foo"
+
 module AppleDevAssetsUtilities
   class ExpirableAppleDevAsset
     attr_reader :expired, :not_valid
@@ -57,23 +60,5 @@ module AppleDevAssetsUtilities
   end 
 end
 
-# in email provisioning bundle id, "profile name" and date of expiration
 
-# unit test this
-# sample/dummy data
-# error paths (eg cannot connect)
-# aget = Cupertino::ProvisioningPortal::Agent.new
-# aget.list_certificates
-# aget.list_certificates(:distribution)
-# aget.list_profiles
-# aget.list_profiles(:distribution)
 
-# ensure $ios login is performed and no need to credentialise
-#
-
-#aget.username = "some.one@there.com"
-#aget.password = "foo"
-
-# z = Certificates.new(aget.list_certificates, Date.today, 57)
-# puts "#{z.expired.size} #{z.not_valid.size}"
-# z = ProvisioningProfiles.new(aget.list_profiles(:distribution), Date.today, 57)
